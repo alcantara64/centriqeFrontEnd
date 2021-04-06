1334cbd72d7a20c593e241d27b681a11d3a895b5
Gaurav Bhārat Mendse <gaurav.mendse@gmail.com>
2021-04-06 12:33:58 +0530

    removed unused setup-table-list css file

--- src/app/dashboard/features/client-setup/load-customer-data/load-customer-data.component.ts
+++ src/app/dashboard/features/client-setup/load-customer-data/load-customer-data.component.ts
@@ -15,35 +15,36 @@ import { SnackbarService } from 'src/app/shared/components/snackbar.service';
 @Component({
   selector: 'app-load-customer-data',
   templateUrl: './load-customer-data.component.html',
-  styleUrls: ['./load-customer-data.component.css', '../../../shared/styling/setup-table-list.shared.css']
+  styleUrls: ['./load-customer-data.component.css'],
 })
-export class LoadCustomerDataComponent extends BaseComponent implements OnInit, OnDestroy {
-
+export class LoadCustomerDataComponent
+  extends BaseComponent
+  implements OnInit, OnDestroy {
   selectedIndex = 0;
   dataSource!: MatTableDataSource<any>;
   private _customerUploadsSub$!: Subscription;
   globalHoldingOrg!: HoldingOrg;
   private _globalHoldingOrgListenerSub$!: Subscription;
-  
 
   /** Cols chosen to be displayed on the table */
-  displayedColumns: string[] = [
-    'name',
-    'size',
-    'action_buttons',
-  ];
-  constructor(private _clientSetupService: ClientSetupService, _loadingService: LoadingService, private _snackbarService: SnackbarService) {
+  displayedColumns: string[] = ['name', 'size', 'action_buttons'];
+  constructor(
+    private _clientSetupService: ClientSetupService,
+    _loadingService: LoadingService,
+    private _snackbarService: SnackbarService
+  ) {
     super(_loadingService);
   }
 
   ngOnInit(): void {
     this.getHoldingOrgFromLocalStorage();
-    this.getCustomerUploads()
- 
+    this.getCustomerUploads();
   }
 
   getCustomerUploads() {
-    const initialLoad$ = this._clientSetupService.getCustomerUploads(this.globalHoldingOrg._id)
+    const initialLoad$ = this._clientSetupService.getCustomerUploads(
+      this.globalHoldingOrg._id
+    );
     this._customerUploadsSub$ = this._loadingService
       .showProgressBarUntilCompleted(initialLoad$, 'query')
       .subscribe(
@@ -56,38 +57,38 @@ export class LoadCustomerDataComponent extends BaseComponent implements OnInit, 
           this._setLoading(false);
         }
       );
-
   }
   onUpload(event: FileList) {
     const file: File = event[0];
     if (file) {
       const formData = new FormData();
-      formData.append('holdingOrg',this.globalHoldingOrg._id);
+      formData.append('holdingOrg', this.globalHoldingOrg._id);
       formData.append('code', this.globalHoldingOrg?.code);
       formData.append('file', file);
-      this._clientSetupService.uploadCustomerData(formData).toPromise().then(res => {
-        this._snackbarService.showSuccess('File Uploaded successfully');
-        this.getCustomerUploads();
-      }).catch(error => {
-        this._snackbarService.showError('fileUpload error');
-
-      });
+      this._clientSetupService
+        .uploadCustomerData(formData)
+        .toPromise()
+        .then((res) => {
+          this._snackbarService.showSuccess('File Uploaded successfully');
+          this.getCustomerUploads();
+        })
+        .catch((error) => {
+          this._snackbarService.showError('fileUpload error');
+        });
     }
   }
 
   ngOnDestroy() {
-    this._customerUploadsSub$.unsubscribe()
+    this._customerUploadsSub$.unsubscribe();
     this._globalHoldingOrgListenerSub$?.unsubscribe();
   }
-  getHoldingOrgFromLocalStorage(){
+  getHoldingOrgFromLocalStorage() {
     const userInfo = localStorage.getItem('userInfo');
-    if(userInfo){
+    if (userInfo) {
       const parsedData = JSON.parse(userInfo);
-      if(parsedData?.holdingOrg){
-       this.globalHoldingOrg = parsedData?.holdingOrg;
+      if (parsedData?.holdingOrg) {
+        this.globalHoldingOrg = parsedData?.holdingOrg;
       }
     }
-
   }
-
 }
